e1b66399ef877b83be4d2d9f7fd731c10ce2d1ca
marcoskleinn <marcos.klein@davinci.edu.ar>
2024-09-09 10:11:13 -0300

    agrego

--- src/ejemplo/main.java
+++ src/ejemplo/main.java
@@ -1,11 +1,13 @@
 package ejemplo;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		JOptionPane.showMessageDialog(null, "ERGEAE");
 		
 	}
 
